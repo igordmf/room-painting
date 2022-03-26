f3ddeebe1f2efb40dfdbe35f1b0a595c92587934
igordmf <igordanielmf@gmail.com>
2022-03-26 16:57:13 -0300

    creating Home component

---
+++ src/components/Home/Home.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  border: 2px solid #fff;
+  border-radius: 7px;
+  padding: 20px;
+`
+
+export const Content = styled.div`
+  display: flex;
+
+  form {
+    max-width: 338px;
+  }
+`
+
+export const Title4 = styled.h4`
+  font-size: 1.2rem;
+`
