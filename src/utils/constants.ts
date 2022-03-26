85427f8874292122f01977cfd0c1a39c972751e0
igordmf <igordanielmf@gmail.com>
2022-03-26 16:10:05 -0300

    required constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,30 @@
+export const MIN_WALL_AREA = 1;
+export const MAX_WALL_AREA = 15;
+
+export const MAX_DOOR_AND_WINDOWS_PERCENTUAL_AREA = 0.5;
+
+export const DOOR_HEIGHT = 1.9;
+export const DOOR_WIDTH = 0.8;
+export const DOOR_DIMENSIONS = DOOR_HEIGHT * DOOR_WIDTH;
+
+export const WALL_DOOR_MIN_DIFF = 0.3;
+
+export const WINDOW_HEIGHT = 1.2;
+export const WINDOW_WIDTH = 2.2;
+export const WINDOW_DIMENSIONS = WINDOW_HEIGHT * WINDOW_WIDTH;
+
+export const SQUARE_METERS_PER_LITER = 5;
+
+export const PAINT_CANS_LITERS_SIZES = {
+  "18": 18,
+  "3.6": 3.6,
+  "2.5": 2.5,
+  "0.5": 0.5,
+}
+
+export const WALL_NAMES_TEXT = {
+  firstWall: "primeira parede",
+  secondWall: "segunda parede",
+  thirdWall: "terceira parede",
+  fourthWall: "quarta parede",
+}
