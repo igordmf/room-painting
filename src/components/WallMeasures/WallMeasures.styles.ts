6eb8d4ef134a140e1cd85cc5fbeec21564abf987
igordmf <igordanielmf@gmail.com>
2022-03-26 16:49:41 -0300

    creating wall measures component

---
+++ src/components/WallMeasures/WallMeasures.styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: Calibri, Arial, sans-serif;
+  font-size: 1.2rem;
+  font-weight: 400;
+  line-height: 1.5rem;
+  margin: 18px 10px 18px 0;
+`
+
+export const WarningText = styled.p`
+  color: #f4901d;
+`
---
+++ src/components/WallMeasures/index.tsx
@@ -0,0 +1,1 @@
+export { WallMeasures } from './WallMeasures';
