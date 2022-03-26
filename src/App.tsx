efb736d4d7afd795894a33bae00cd38e015abe0a
igordmf <igordanielmf@gmail.com>
2022-03-26 16:58:02 -0300

    app component adn styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import './App.css';
+import { GlobalStyle } from './styles/global.styles';
+import AppProvider from './context';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+import { AppStyles, BgStyles } from './styles/App';
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+      <div className="App" style={AppStyles}>
+        <Header />
+
+        <Home />
+      </div>
+  
+      <div title='bg-image' style={BgStyles}/>
+
+      <GlobalStyle />
+    </AppProvider>
   );
 }
 
