71be8a6d0749da57e9a398588f6f498195cf6b2d
igordmf <igordanielmf@gmail.com>
2022-03-26 16:50:09 -0300

    creating requirements component

---
+++ src/components/Requirements/Requirements.style.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  border: 1px solid #EB5B28;
+  border-radius: 8px;
+  line-height: 1.5rem;
+  margin: 26px 0 15px 30px;
+  padding: 12px;
+  width: 360px;
+
+  ul {
+    list-style: circle;
+    list-style-position: inside;
+  }
+`
